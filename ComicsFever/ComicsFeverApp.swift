30dfcd09d7030be6675fab0149ab6e93934bee40
themaripool <marielama@outlook.com.br>
2021-05-02 17:01:24 -0300

    Initial Commit

---
+++ ComicsFever/ComicsFeverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicsFeverApp.swift
+//  ComicsFever
+//
+//  Created by Mariela Andrade on 02/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ComicsFeverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
